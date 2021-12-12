5c84bf01167e7a5c46ad02e655fbb3f53330b1ac
Cmoyaa <81742799+Cmoyaa@users.noreply.github.com>
2021-12-12 08:16:04 -0300

    Implementación Redes sociales

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,5 +52,25 @@ let newSwiper = new Swiper(".adornos-swiper", {
         992: {
           spaceBetween: 80,
         },
-      },
-  });
+    },
+});
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
