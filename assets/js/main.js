a296b79d0451fd58aa0a3765984e7121caadc82e
Cmoyaa <81742799+Cmoyaa@users.noreply.github.com>
2021-12-12 06:32:57 -0300

    Version 1.0

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+const menu = document.getElementById('nav-menu'),
+      abrirMenu = document.getElementById('nav-toggle'),
+      cerrarMenu = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(abrirMenu){
+    abrirMenu.addEventListener('click', () =>{
+        menu.classList.add('show-menu')
+    })
+}
+
+if(cerrarMenu){
+    cerrarMenu.addEventListener('click', () =>{
+        menu.classList.remove('show-menu')
+    })
+}
+/* ---------------------------------------------------------- */
+
+/* Funcion para eliminar el menu desplegable */
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+/* ---------------------------------------------------------- */
+
+/* Background */
+function scrollHeader(){
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 50) 
+        header.classList.add('scroll-header'); 
+    else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
